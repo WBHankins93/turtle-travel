f707ed6b8768a53ed888f6bd105f2bb405a5d231
Ben Hankins <benhankins@Bens-MBP.lan>
2024-12-27 16:09:33 -0600

    intialized server

---
+++ server/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const postRoutes = require('./routes/posts');
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(bodyParser.json());
+
+// Routes
+app.use('api/posts', postRoutes);
+
+const port = 5000;
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`)
+})
