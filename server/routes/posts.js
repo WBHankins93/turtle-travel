a4ba849fd3d1bde2c0a1556f55ec0bdba9954342
Ben Hankins <benhankins@Bens-MBP.lan>
2024-12-27 16:20:36 -0600

    setting up backend routes complete

---
+++ server/routes/posts.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const router = express.Router();
+
+let posts = [];
+
+
+// Create new post (CREATE)
+router.post('/', (req,res) => {
+    const { title, content } = req.body;
+    const newPost = { title, content, id: Date.now() };
+    posts.push(newPost);
+    res.status(201).json(newPost);
+})
+
+
+// Get all posts (READ)
+router.get('/', (req,res) => {
+    res.status(200).json(posts);
+})
+
+
+// Update post (UPDATE)
+router.put('/:id', (req,res) => {
+    const { id } = req.params;
+    const { title, content } = req.body;
+
+    const postIndex = posts.findIndex(post => post.id == id);
+    if (postIndex !== -1) {
+        post[postIndex] = {...post[postIndex, title, content]};
+        res.status(200).json(posts[postIndex])
+    } else {
+        res.status(404).json({message: "Post not found"});
+    }
+});
+
+// Delete post (DELETE)
+router.delete('/:id', (req, res) => {
+    const { id } = req.params;
+    posts = posts.filter(post => post.id != id);
+    res.status(204).end();
+})
+
+module.exports = router;
